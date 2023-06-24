1172131232944fb4307f2f31347aeef48bd72699
Veronika <nikazemlakova38@gmail.com>
2023-06-25 01:05:14 +0300

    rooms

--- server/app.js
+++ server/app.js
@@ -24,18 +24,31 @@ app.listen(port, async () => {
 
 // запуск сокет сервера
 io.on('connection', (socket) => {
-    socket.emit('connected', {
-        message: 'You`re connected succesfully'
-    })
-
-    socket.on('message', (arg) => {
-        console.log(arg);
-    })
-
-    socket.on('disconnect', (reason) => {
-        console.log('Client has been disabled');
-        console.log(reason);
-    })
+  socket.on('message', (data) => {
+    socket.join('room:' + data.room_id)
+    io.to('room:' + data.room_id).emit('message', data.message)
+  })
 });
 
+/* setInterval( () => {
+    io.emit('ping', {
+    ts: (new Date)
+  })
+}, 10000) */
+
+
 httpServer.listen(3001);
+
+
+/* socket.emit('connected', {
+  message: 'You`re connected succesfully'
+})
+
+socket.on('message', (arg) => {
+  console.log(arg);
+})
+
+socket.on('disconnect', (reason) => {
+  console.log('Client has been disabled');
+  console.log(reason);
+}) */
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <input type="number" v-model="room_id">
     <input type="text" v-model="message">
     <button @click="send" class="btn">Send</button>
     <h3>Installed CLI Plugins</h3>
@@ -35,6 +36,14 @@
 const { io } = require("socket.io-client");
 const socket = io("http://localhost:3001");
 
+socket.on('message', (data) => {
+  console.log(data);
+})
+
+socket.on('ping', (data) => {
+  console.log(data);
+})
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -42,12 +51,16 @@ export default {
   }, 
   data() {
     return {
-      message: null
+      message: null,
+      room_id: 1
     }
   }, 
   methods: {
     send() {
-      socket.emit('message', this.message)
+      socket.emit('message', {
+        message: this.message,
+        room_id: this.room_id
+      })
     }
   }
 }
